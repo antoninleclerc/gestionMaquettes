197173ca346853c7a9ca94d92b0c5123a58a9832
MichaelGerwill <32921972+MichaelGerwill@users.noreply.github.com>
2017-12-12 11:38:52 +0100

    Suppression méthodes
    Suppression Code() Intitulé() Coefficient()

--- UE.h
+++ UE.h
@@ -9,21 +9,19 @@ class UE : public Cours
 	public:
 		UE();
 		UE(const std::string &code, const std::string &intitule, int coefficient, int CM, int TD, int TP, int ECTS);
-		virtual string code() const override;
-		virtual string intitule() const override;
-		virtual int coefficient() const override;
+		
 		int ECTS() const;
 		int nbECUE() const;
 		int nbCMTotal() const;
 		int nbTDTotal() const;
 		int nbTPTotal() const;
-				
+			
 		virtual int nbHeuresTotal() const override;
 		virtual void afficher(std::ostream &ost) const override;
 		
 	private:
 		int d_ECTS;
-		vector <ECUE*> d_UE; //La première case du vecteur est toujours l'UE principale, tout ce qui suit est les ECUE de cet UE.
+		vector <ECUE*> d_UE; //La premiÃ¨re case du vecteur est toujours l'UE principale, tout ce qui suit est les ECUE de cet UE.
 };
 
 #endif
