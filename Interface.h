99965f6c1d58ad82fe33bcec34704c1e6fca551d
Lydii07 <33064237+Lydii07@users.noreply.github.com>
2017-12-14 12:36:17 +0100

    Create Interface.h

---
+++ Interface.h
@@ -0,0 +1,15 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+class Interface{
+	public:
+		Interface();
+		void menuPrincipal();
+		void menuFormation();
+		void menuMaquette();
+		void menuUE();
+    void menuUEchoix();
+	
+};
+
+#endif
