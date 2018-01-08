c36163c268ffb1b10a4c87a4af3c6cdae81e43d5
MichaelGerwill <32921972+MichaelGerwill@users.noreply.github.com>
2018-01-08 15:40:51 +0100

    Correction erreurs

--- UEchoix.cpp
+++ UEchoix.cpp
@@ -3,22 +3,23 @@
 
 UEchoix::UEchoix(const std::string &code, const std::string &intitule, int coefficient, int CM, int TD, int TP, int ECTS)
 {
-    UEchoix* ueChoix = new UE{code, intitule, coefficient, CM, TD, TP, ECTS};
+    UE* ueChoix = new UE{code, intitule, coefficient, CM, TD, TP, ECTS};
     d_UEchoix.push_back(ueChoix);
 }
 
 UEchoix::~UEchoix()
 {}
 
-virtual void UEchoix::affiche(std::ostream &ost) const override
+void UEchoix::affiche(std::ostream &ost) const
 {
     ost<<std::setw(4)<<code()<<std::setw(4)<<coefficient()<<std::setw(4)<<ECTS()<<std::setw(4)<<"UE CHOIX"<<std::endl;
     for(int i=0; i<d_UEchoix.size()-1; i++)
     {
-        d_UEchoix[i].affiche()<<std::endl;
+        d_UEchoix[i]->affiche(ost);
+        ost<<std::endl;
         ost<<std::setw(16)<<"OU"<<std::endl;
     }
-    d_UEchoix[d_UEchoix.size()-1].affiche();
+    d_UEchoix[d_UEchoix.size()-1]->affiche(ost);
 }
 
 void UEchoix::ajouterUE(UE *ue)
@@ -29,7 +30,7 @@ void UEchoix::ajouterUE(UE *ue)
 bool UEchoix::supprimerUE(const std::string& code)
 {
     int i=0;
-    while(i<d_UEchoix().size() && d_UEchoix[i].code() != code)
+    while(i<d_UEchoix.size() && d_UEchoix[i]->code() != code)
     {
         i++;
     }
