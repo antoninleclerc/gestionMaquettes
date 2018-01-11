306cc60cdcdae1915e26035b8221a0b5db29673e
esoz1 <33064218+esoz1@users.noreply.github.com>
2018-01-11 10:44:17 +0000

    Update formation.cpp

--- formation.cpp
+++ formation.cpp
@@ -38,10 +38,10 @@ void formation::modifierIntitule(std::string intitule)
 
 bool formation::supprimerMaquette(int numeroSemestre)
 {
-	if (numeroSemestre <= d_formations.size())
+	if (numeroSemestre <= d_maquettes.size())
 	{
-		delete d_formations[numeroSemestre-1] ; 
-		for (int i = numeroSemestre; i < d_formations.size(); i++) 
+		delete d_maquettes[numeroSemestre-1] ; 
+		for (int i = numeroSemestre; i < d_maquettes.size(); i++) 
 		{
 			d_maquettes[i-1] = d_maquettes[i];
 		}
@@ -51,4 +51,13 @@ bool formation::supprimerMaquette(int numeroSemestre)
 	{
 		return false; 
 	}	
+}
+void formation :: afficheFormation(std :: ostream & ost) const
+{
+	ost << std :: setw(4) << d_intitule << std :: endl ;
+	for (int i=0 ; i< d_maquettes.size() ; i++ )
+	{
+		ost << std::setw(4)<< " Semestre " << i+1 <<std ::endl ;
+		d_maquettes.afficheMaquette (ost) ; 
+	}	ost<< std :: endl ; 
 }
