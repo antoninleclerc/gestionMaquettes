d879d7e7b6e931a0f759ec60eeb519f5308c0677
antoninleclerc <32922056+antoninleclerc@users.noreply.github.com>
2018-01-13 21:07:46 +0100

    Correction documentation

--- ECUE.h
+++ ECUE.h
@@ -3,19 +3,19 @@
 
 #include "Cours.h"
 
-/**	\class ECUE
- *	\brief classe représentant une ECUE
+/**	@class ECUE
+ *	@brief classe représentant une ECUE
  */
 class ECUE : public Cours {
 	public:
 		/**	Constructeur d'une ECUE
 		 *	@brief Constructeur d'ECUE à partir d'un code, d'un intitulé, d'un coefficient, des heures CM, des heures TD et des heures TP
-		 *	@param code : code d'une ECUE
-		 *	@param intitule : l'intitule d'une ECUE
-		 *	@param coefficient : coefficient d'une ECUE
-		 * 	@param heuresCM : nombre d'heures de CM d'une ECUE
-		 *	@param heuresTD : nombre d'heures de TD d'une ECUE
-		 	@param heuresTP : nombre d'heures de TP d'une ECUE
+		 *	@param[in] code : code d'une ECUE
+		 *	@param[in] intitule : l'intitule d'une ECUE
+		 *	@param[in] coefficient : coefficient d'une ECUE
+		 * 	@param[in] heuresCM : nombre d'heures de CM d'une ECUE
+		 *	@param[in] heuresTD : nombre d'heures de TD d'une ECUE
+		 	@param[in] heuresTP : nombre d'heures de TP d'une ECUE
 		 */
 		ECUE(const std::string &code, const std::string &intitule, int coefficient, int heuresCM, int heuresTD, int heuresTP);
 		
@@ -40,19 +40,19 @@ class ECUE : public Cours {
 		
 		/**	Fonction modifierHeuresCM
 		 *	Permet de modifier le nombre d'heures CM d'une ECUE
-		 *	@param heuresCM Le nouveau nombre d'heures CM de l'ECUE
+		 *	@param[in] heuresCM Le nouveau nombre d'heures CM de l'ECUE
 		 */
 		void modifierHeuresCM(int heuresCM);
 		
 		/**	Fonction modifierHeuresTD
 		 *	Permet de modifier le nombre d'heures TD d'une ECUE
-		 *	@param heuresCM Le nouveau nombre d'heures TD de l'ECUE
+		 *	@param[in] heuresCM Le nouveau nombre d'heures TD de l'ECUE
 		 */
 		void modifierHeuresTD(int heuresTD);
 		
 		/**	Fonction modifierHeuresTP
 		 *	Permet de modifier le nombre d'heures TP d'une ECUE
-		 *	@param heuresCM Le nouveau nombre d'heures TP de l'ECUE
+		 *	@param[in] heuresCM Le nouveau nombre d'heures TP de l'ECUE
 		 */
 		void modifierHeuresTP(int heuresTP);
 		
