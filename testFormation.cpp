a13076c3cc736d51cbd29a048b6d67c067beb36e
esoz1 <33064218+esoz1@users.noreply.github.com>
2018-01-14 16:44:28 +0100

    Add files via upload

---
+++ testFormation.cpp
@@ -0,0 +1,57 @@
+#include "doctest.h"
+#include "formation.h" 
+
+
+TEST_CASE {"Construction d'une formation "}
+{
+	int nbAnnee=3 ; 
+	std:: string domaine="fst" ; 
+	std:: string mention= "Licence Informatique" ; 
+	std::string parcours = "Informatique" ;
+	formation f {nbAnnee, domaine, mention, parcours} ; 
+	REQUIRE_EQ(f.nombreAnnee(), nbAnnee) ; 
+	REQUIRE_EQ(f.domaine(), domaine) ; 
+	REQUIRE_EQ(f.mention(), mention) ; 
+	REQUIRE_EQ(f.parcours(), parcours) ; 
+}
+
+TEST_CASE("Ajout d'une maquette")
+{
+	maquette m ; 
+	ajouterMaquette(m) ; 
+	REQUIRE_EQ(maquetteNumero(nombreMaquettes), m) ; 
+}
+
+TEST_CASE("Modification d'une formation")
+{
+	int nbAnnee=3 ; 
+	std:: string domaine="fst" ; 
+	std:: string mention= "Licence Informatique" ; 
+	std::string parcours = "Informatique" ;
+	formation f {nbAnnee, domaine, mention, parcours} ; 
+	SUBCASE{"nombre d'année modifié"}
+	{
+		f.modifierNombreAnnee(2) ; 
+		REQUIRE_EQ(f.nombreAnnee(), 2) ;
+	}
+	SUBCASE{"Domaine modifié"}
+	{
+		f.modifierDomaine ("flsh") ; 
+		REQUIRE_EQ(f.domaine(),"flsh" ) ;
+	}
+	SUBCASE{"Mention modifié"}
+	{
+		f.modifierMention("Licence Anglais") ; 
+		REQUIRE_EQ(f.mention(), "Licence Anglais") ;
+	}
+	SUBCASE{"Parcours modifié"}
+	{
+		f.modifierParcours("Anglais") ; 
+		REQUIRE_EQ(f.parcours(),"Anglais" ) ;
+	}
+	
+}
+
+
+
+
