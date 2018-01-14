0a48099ee2bad05186d3c251f9fe8c6dd83651bb
esoz1 <33064218+esoz1@users.noreply.github.com>
2018-01-14 11:09:50 +0100

    Update Maquette.h

--- Maquette.h
+++ Maquette.h
@@ -10,14 +10,35 @@ namespace GestionMaquettes
 	class Maquette 
 	{
 		public : 
+			/**	Constructeur d'une maquette
+			*/
 			Maquette();
+			/**	Destructeur d'une maquette
+			*/
 			~Maquette();
+			/** Fonction ajouterUE
+			*	Permet d'ajouter une UE au vecteur d'UE choix
+			*	@param[in] ue - Pointeur sur l'ue à ajouter
+			*/
 			void ajouterUE(UE * ue); 
+			/** Fonction affiche
+       			 *	@Permet d'afficher une maquette
+       			 */
 			void afficheMaquette(std::ostream & ost) const; 
+			/** Fonction supprimerUE
+       			*	Retourne vrai si la suppression de l'UE s'est bien passee
+        		*	@param[in] code - chaine de caractère représentant le code de l'UE a supprrimer
+      			*/
 			bool supprimerUE(std::string & code);
+			/** Fonction nombreUEDansMaquette
+        		 *	@retourne le nombre d'UE dans une maquette
+         		*/
 			int nombreUEDansMaquette() const; 
 				
 		private :
+			/**
+			*	vecteur de pointeur sur UE 
+			*/
 			std::vector<UE*> d_UE; 
 	};
 	
