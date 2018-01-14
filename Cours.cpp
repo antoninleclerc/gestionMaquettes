604b494cf5aee33d82b0bf27161b5d48c9f1b7e1
MichaelGerwill <32921972+MichaelGerwill@users.noreply.github.com>
2018-01-15 00:18:37 +0100

    Mise en commun

--- Cours.cpp
+++ Cours.cpp
@@ -1,32 +1,35 @@
 #include "Cours.h"
 
-
-Cours::Cours(const std::string& code, const std::string& intitule, int coefficient) :
-    d_code{code},
-    d_intitule{intitule},
-    d_coefficient{coefficient}
-{}
-
-Cours::~Cours()
-{}
-
-std::string Cours::code()const
+namespace GestionMaquettes
 {
-    return d_code;
+	Cours::Cours(const std::string& code, const std::string& intitule, int coefficient) :
+	    d_code{code},
+	    d_intitule{intitule},
+	    d_coefficient{coefficient}
+	{}
+	
+	Cours::~Cours()
+	{}
+	
+	std::string Cours::code()const
+	{
+	    return d_code;
+	}
+	
+	std::string Cours::intitule()const
+	{
+	    return d_intitule;
+	}
+	
+	int Cours::coefficient()const
+	{
+	    return d_coefficient;
+	}
+	
+	std::ostream& operator<<(std::ostream& ost, const Cours& c)
+	{
+		c.affiche(ost);
+		return ost;
+	}
 }
-
-std::string Cours::intitule()const
-{
-    return d_intitule;
-}
-
-int Cours::coefficient()const
-{
-    return d_coefficient;
-}
-
-std::ostream& operator<<(std::ostream& ost, const Cours& c)
-{
-	c.affiche(ost);
-	return ost;
-}
+	
--- Cours.h
+++ Cours.h
@@ -2,56 +2,46 @@
 #define COURS_H
 #include <string>
 #include <iostream>
-class Cours
+
+namespace GestionMaquettes
 {
-    public:
-        /** Constructeur d'un cours
-		 *	@brief Constructeur de cours à partir d'un code, d'un intitule et d'un coefficient
-		 *	@param[in] code - chaine de caractère représentant le code
-		 *	@param[in] intitule - chaine de caractère représentant l'intitulé
-		 *	@param[in] coefficient - entier représentant le coefficient
-		 */
-        Cours(const std::string& code, const std::string& intitule, int coefficient);
-        
-        /** destructeur de cours
-        */
-        virtual ~Cours();
-        
-        /** Fonction code
-         *	@retourne le code d'un cours
-         */
-        std::string code()const;
-        
-        /**	Fonction intitule
-       	 *	@Retourne l'intitule d'un cours
-         */
-        std::string intitule()const;
-        
-        /** Fonction coefficient
-         *	@Retourne le coefficient d'un cours
-         */
-        int coefficient()const;
-        
-        /** Fonction affiche
-       	 *	@Permet d'afficher le contenu d'un cours
-         */
-        virtual void affiche(std::ostream& ost) const = 0;
+	class Cours
+	{
+	    public:
+	        /*
+	        Constructeur à partir d'un code, un intitulé et un coefficient
+	        **/
+	        Cours(const std::string& code, const std::string& intitule, int coefficient);
+	        /*
+	        destructeur de cours
+	        **/
+	        virtual ~Cours();
+	        /*
+	        @retourne le code d'un cours
+	        **/
+	        std::string code()const;
+	        /*
+	        @retourne l'intitulé d'un cours
+	        **/
+	        std::string intitule()const;
+	        /*
+	        @retourne le coefficient d'un cours
+	        **/
+	        int coefficient()const;
+	        /*
+	        affiche le contenu d'un cours
+	        **/
+	        virtual void affiche(std::ostream& ost) const = 0;
+	
+	    private:
+	        std::string d_code;
+	        std::string d_intitule;
+	        int d_coefficient;
+	};
+	
+	std::ostream& operator<<(std::ostream& ost, const Cours& s);
+}
 
-    private:
-    	/**
-		 *Code d'un cours
-		 */
-        std::string d_code;
-        /**
-         *Intitule d'un cours
-         */
-        std::string d_intitule;
-        /**
-		 *Coefficient d'un cours
-		 */
-        int d_coefficient;
-};
 
-std::ostream& operator<<(std::ostream& ost, const Cours& s);
 
 #endif // COURS_H
