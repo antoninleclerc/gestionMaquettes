599ab0bc968bcd554ffe5db6899ea236a68ba26c
esoz1 <33064218+esoz1@users.noreply.github.com>
2018-01-15 14:37:10 +0100

    Update testMaquette.cpp

--- testMaquette.cpp
+++ testMaquette.cpp
@@ -4,13 +4,7 @@
 
 TEST_CASE("Ajout d'une ue")
 {
-	UE ue ; 
-	ajouterUE(u) ; 
-	REQUIRE_EQ(UeNumero(nombreUEDansMaquette()), ue) ; 
-}
-
-TEST_CASE("Suppression d'une UE")
-{
+	Maquette m;
 	std :: code = "1121" ; 
 	std:: string intitule = "Algo" ; 
 	int CM = 14 ; 
@@ -18,10 +12,24 @@ TEST_CASE("Suppression d'une UE")
 	int TP = 18 ; 
 	int ECTS = 3 ; 
 	UE ue {code, intitule, coefficient, CM, TD, TP, ECTS} ;
-	ajouterUE(ue) ;  
-	if (supprimerUE(code) == true) 
+	m.ajouterUE(ue) ; 
+	REQUIRE(m.UeNumero(m.nombreUEDansMaquette())== ue) ; 
+}
+
+TEST_CASE("Suppression d'une UE")
+{
+	Maquette  m ; 
+	std :: code = "1121" ; 
+	std:: string intitule = "Algo" ; 
+	int CM = 14 ; 
+	int TD = 15 ;
+	int TP = 18 ; 
+	int ECTS = 3 ; 
+	UE ue {code, intitule, coefficient, CM, TD, TP, ECTS} ;
+	m.ajouterUE(ue) ;  
+	if (m.supprimerUE(code) == true) 
 	{
-		REQUIRE(UeNumero(nombreUEDansMaquette().code()!=code ) ;
+		REQUIRE(m.UeNumero(m.nombreUEDansMaquette().code()!=code ) ;
 	 } 
 	
 	
